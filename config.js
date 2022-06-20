ad9435e5f383220eb34e1fead2dd3ee2ea31ed12
Niximkk <85513545+Niximkk@users.noreply.github.com>
2022-06-20 00:23:06 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+const config = {
+    
+    USERNAME: "🌸 Nixim o Guaxinim#0666",
+    /**
+     * This can either be a file link (relative or external)
+     * or a Gravatar email or hash.
+     */
+    PICTURE: "assets/img/profile_picture.gif",
+
+    DESCRIPTION: `Olá, bem vindo ao meu perfil! :)`,
+
+    BADGES: [
+        { id: "Nitro Sub", image: "assets/img/nitro_sub.svg" },
+        { id: "Booster", image: "assets/img/booster.svg" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/partner.png", link: "https://discord.com", name: "Essa página aqui." },
+        { id: "project-2", image: "assets/img/raccstore.jpg", link: "https://discord.gg/yuHQkZuY8m", name: "Minha lojinha de contas." }
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-xbox", link: "http://live.xbox.com/Profile?Gamertag=Niximkk" },
+        { class_name: "fab fa-discord", link: "https://discordapp.com/users/973010982282596412" },
+        { class_name: "fab fa-steam", link: "https://steamcommunity.com/id/Niximkk" },
+    ],
+
+}
