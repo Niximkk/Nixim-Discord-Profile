ee186f41fe5852ae5247a4c486773d8f72d38c26
Nixim o Guaxinim <85513545+Niximkk@users.noreply.github.com>
2022-06-22 23:13:14 -0300

    Adicionando o Script de Confete

---
+++ assets/confetti.js
@@ -0,0 +1,61 @@
+function basic(){
+    confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { y: 0.6 }
+  });
+  }
+  
+  function randomDirection(){
+   confetti({
+    angle: randomInRange(55, 125),
+    spread: randomInRange(50, 70),
+    particleCount: randomInRange(50, 100),
+    origin: { y: 0.6 }
+  }); 
+  }
+  
+  function makeItRain() {
+    document.getElementById("Partybtn").disabled = true;
+    var end = Date.now() + (111 * 1000);
+  
+  // go Buckeyes!
+  var colors = ['#6064f4', '#ffffff', '#f595be'];
+  
+  function frame() {
+    confetti({
+      particleCount: 3,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+      colors: colors
+    });
+    confetti({
+      particleCount: 3,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+      colors: colors
+    });
+  
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+    else {
+      document.getElementById("makeItRain").disabled = false;
+    }
+  };
+    frame();
+  }
+  
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  var aud = document.getElementById("Partybtn").children[0];
+
+        function Partybtn() {
+        aud.play();
+        aud.volume = 0.07
+        makeItRain();
+        }
